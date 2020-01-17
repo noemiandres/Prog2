392aea2f544569298802e82fd1d086d9052075ee
noemiandres <55581656+noemiandres@users.noreply.github.com>
2020-01-17 22:36:31 +0100

    Neuste Version, Rezeptanpassung

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ rezepte = {
             "Zitrone": (1, ""),
             "Zucker": (140,"g")
         },
-        "Anleitung": ("Für den schnellen Apfelkuchen zuerst den Backofen Ober- und Unterhitze auf 180 Grad vorheizen."," Danach das Eiweiss in einer Schüssel zu steifen Schnee schlagen.","In einer anderen Schüssel das Eigelb mit Zucker schaumig schlagen.", "Wasser und Zitronenschale dazugeben.","Das mit Backpulver versiebte Mehl untermischen und den Eischnee vorsichtig unterheben.", "Danach die Äpfel schälen und das Kerngehäuse entfernen und in Spalten schneiden.","Die Teig-Masse in eine gefettete Springform füllen.", " Apfelspalten auf dem Teig verteilen und mit Zimt und Zucker bestreuen.", "Den Kuchen im vorgeheizten Backofen bei 180 Grad etwa 40 Minuten backen."," En guete!"),
+        "Anleitung": ("Der Apfelkuchen ist für 4 Leute gedacht.", "Für den schnellen Apfelkuchen zuerst den Backofen Ober- und Unterhitze auf 180 Grad vorheizen."," Danach das Eiweiss in einer Schüssel zu steifen Schnee schlagen.","In einer anderen Schüssel das Eigelb mit Zucker schaumig schlagen.", "Wasser und Zitronenschale dazugeben.","Das mit Backpulver versiebte Mehl untermischen und den Eischnee vorsichtig unterheben.", "Danach die Äpfel schälen und das Kerngehäuse entfernen und in Spalten schneiden.","Die Teig-Masse in eine gefettete Springform füllen.", " Apfelspalten auf dem Teig verteilen und mit Zimt und Zucker bestreuen.", "Den Kuchen im vorgeheizten Backofen bei 180 Grad etwa 40 Minuten backen."," En guete!"),
         "Bild": "img/apfelkuchen.jpg"
     },
     "Kartoffelstock": { #Daten für Kartoffelstock
